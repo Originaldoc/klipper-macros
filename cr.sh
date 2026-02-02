d82aad8ee9eb734a159a72dfc013f759d540607b
Originaldoc <originaldoc@hotmail.com>
2026-02-02 18:09:45 +0100

    RESUME_CANCEL working

--- cr.sh
+++ cr.sh
@@ -1,38 +1,101 @@
 #!/bin/bash
-SD_PATH=/home/dietpi/printer_data/gcodes
-FILENAME=cr.gcode
-cat ${SD_PATH}/${3} > /tmp/crtmpA.$$
-echo 'G28 X Y' > ${SD_PATH}/${FILENAME}
-echo "SET_KINEMATIC_POSITION Z=${1}" >> ${SD_PATH}/${FILENAME}
-#cat /tmp/crtmpA.$$ | sed -e '1,/Z'${2}'/ d' | sed -ne '/ Z/,$ p' | grep -m 1 ' Z' | sed -ne 's/.* Z\([^ ]*\)/G0 Z\1/p' >> ${SD_PATH}/${FILENAME}
-BG_EX=`tac /tmp/crtmpA.$$ | sed -e '/ Z'${2}'[^0-9]*$/q' | tac | tail -n+2 | sed -e '/ Z[0-9]/ q' | tac | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p'`
-# If we failed to match an extrusion command (allowing us to correctly set the E axis) prior to the matched layer height, then simply set the E axis to the first E value present in the resemued gcode.
-# This avoids extruding a huge blod on resume, and/or max extrusion errors.
-if [ "${BG_EX}" = "" ]; then
-  BG_EX=`tac /tmp/crtmpA.$$ | sed -e '/ Z'${2}'[^0-9]*$/q' | tac | tail -n+2 | sed -ne '/ Z/,$ p' | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p'`
+
+SD_PATH="$HOME/printer_data/gcodes"
+OUTFILE="$SD_PATH/cr.gcode"
+TEMPFILE="$SD_PATH/crtmp.gcode"
+
+Z_OFFSET="$1"
+Z_HEIGHT="$2"
+SRCFILE="$3"
+BED_TEMP="$4"
+HOT_TEMP="$5"
+
+cp "$SD_PATH/$SRCFILE" "$TEMPFILE"
+
+############################################
+# 1. BUSCAR LÍNEA REAL DE REANUDACIÓN
+############################################
+RESUME_LINE=$(awk -v zh="$Z_HEIGHT" '
+{
+  if ($0 ~ /[[:space:]]Z/) {
+    line = $0
+    sub(/.*[[:space:]]Z/, "", line)
+    sub(/[[:space:]].*/, "", line)
+    last_z = line + 0
+  }
+
+  if ($0 ~ /[[:space:]]E[-+]?[0-9]/) {
+    if (last_z >= zh) {
+      print NR
+      exit
+    }
+  }
+}
+' "$TEMPFILE")
+
+if [ -z "$RESUME_LINE" ]; then
+  echo "ERROR: No se encontró punto válido de reanudación (Z >= $Z_HEIGHT con extrusión)"
+  exit 1
 fi
-echo ${BG_EX} >> ${SD_PATH}/${FILENAME}
-echo 'G91' >> ${SD_PATH}/${FILENAME}
-echo "G1 Z-${1}" >> ${SD_PATH}/${FILENAME}
-echo 'G90' >> ${SD_PATH}/${FILENAME}
-cat /tmp/crtmpA.$$ | sed -e '1,/Z'${2}'/ d' | sed -ne '/ Z/,$ p' | grep -m 1 ' Z' | sed -ne 's/.* Z\([^ ]*\)/SET_KINEMATIC_POSITION Z=\1/p' >> ${SD_PATH}/${FILENAME}
-cat /tmp/crtmpA.$$ | sed '/ Z'${2}'/q' | sed -ne '/\(M104\|M140\|M109\|M190\|M106\)/p' >> ${SD_PATH}/${FILENAME}
-if [ -z "${4}" ]; then # (doc) if overwrite BED temp
-  cat /tmp/crtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_bed_temperature | sed -ne 's/.* = /M140 S/p' | head -1 >> ${SD_PATH}/${FILENAME}
-  cat /tmp/crtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_bed_temperature | sed -ne 's/.* = /M190 S/p' | head -1 >> ${SD_PATH}/${FILENAME}
-else
-  echo "M140 S${4}" >> ${SD_PATH}/${FILENAME}  
-  echo "M190 S${4}" >> ${SD_PATH}/${FILENAME}  
-fi  
-if [ -z "${5}" ]; then # (doc) if overwrite HOTEND temp
-  cat /tmp/crtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_print_temperature | sed -ne 's/.* = /M104 S/p' | head -1 >> ${SD_PATH}/${FILENAME}
-  cat /tmp/crtmpA.$$ | sed -ne '/;End of Gcode/,$ p' | tr '\n' ' ' | sed -ne 's/ ;[^ ]* //gp' | sed -ne 's/\\\\n/;/gp' | tr ';' '\n' | grep material_print_temperature | sed -ne 's/.* = /M109 S/p' | head -1 >> ${SD_PATH}/${FILENAME}
-else
-  echo "M104 S${5}" >> ${SD_PATH}/${FILENAME} 
-  echo "M109 S${5}" >> ${SD_PATH}/${FILENAME} 
-fi
-# cat /tmp/crtmpA.$$ | sed -e '1,/ Z'${2}'[^0-9]*$/ d' | sed -e '/ Z/q' | tac | grep -m 1 ' E' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p' >> ${SD_PATH}/${FILENAME}
-#tac /tmp/crtmpA.$$ | sed -e '/ Z'${2}'[^0-9]*$/q' | tac | tail -n+2 | sed -e '/ Z[0-9]/ q' | tac | sed -e '/ E[0-9]/ q' | sed -ne 's/.* E\([^ ]*\)/G92 E\1/p' >> ${SD_PATH}/${FILENAME}
-# cat /tmp/crtmpA.$$ | sed -e '1,/Z'${2}'/ d' | sed -ne '/ Z/,$ p' >> ${SD_PATH}/${FILENAME}
-tac /tmp/crtmpA.$$ | sed -e '/ Z'${2}'[^0-9]*$/q' | tac | tail -n+2 | sed -ne '/ Z/,$ p' >> ${SD_PATH}/${FILENAME}
-rm /tmp/crtmpA.$$
+
+############################################
+# 2. Z REAL
+############################################
+REAL_Z=$(awk -v stop="$RESUME_LINE" '
+NR <= stop {
+  if ($0 ~ /[[:space:]]Z/) {
+    line = $0
+    sub(/.*[[:space:]]Z/, "", line)
+    sub(/[[:space:]].*/, "", line)
+    z = line
+  }
+}
+END { print z }
+' "$TEMPFILE")
+
+echo "DEBUG: RESUME_LINE=$RESUME_LINE  REAL_Z=$REAL_Z"
+
+############################################
+# 3. EXTRUSIÓN
+############################################
+BG_EX=$(sed -n "1,${RESUME_LINE}p" "$TEMPFILE" \
+  | grep ' E' \
+  | tail -n 1 \
+  | sed -n 's/.* E\([^ ]*\)/G92 E\1/p')
+
+############################################
+# 4. CABECERA SEGURA
+############################################
+{
+echo "; === SAFE RESUME (Orca Slicer) ==="
+echo "SET_VELOCITY_LIMIT VELOCITY=50 ACCEL=500"
+echo "G28 X Y"
+echo "SET_KINEMATIC_POSITION Z=$Z_OFFSET"
+[ -n "$BG_EX" ] && echo "$BG_EX"
+echo "G91"
+echo "G1 Z-$Z_OFFSET F300"
+echo "G90"
+echo "SET_KINEMATIC_POSITION Z=$REAL_Z"
+echo "G1 F1200"
+} > "$OUTFILE"
+
+############################################
+# 5. TEMPERATURAS
+############################################
+[ -n "$BED_TEMP" ] && {
+  echo "M140 S$BED_TEMP"
+  echo "M190 S$BED_TEMP"
+} >> "$OUTFILE"
+
+[ -n "$HOT_TEMP" ] && {
+  echo "M104 S$HOT_TEMP"
+  echo "M109 S$HOT_TEMP"
+} >> "$OUTFILE"
+
+############################################
+# 6. CONTINUAR GCODE
+############################################
+sed -n "${RESUME_LINE},\$p" "$TEMPFILE" >> "$OUTFILE"
+
+echo "Resume GCODE listo: $OUTFILE"
+rm "$TEMPFILE"
